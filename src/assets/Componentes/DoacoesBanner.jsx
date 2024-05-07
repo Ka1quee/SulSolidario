73b5cd5e11c7f82cfb539ee06a3b637ba2957bf1
Jessica Magalhães da silva <112392689+Jesmgl@users.noreply.github.com>
2024-05-07 10:38:37 -0300

    componente DoacoesBanner

---
+++ src/assets/Componentes/DoacoesBanner.jsx
@@ -0,0 +1,13 @@
+import Styles from '../Componentes/DoacoesBanner.module.css'
+
+function DoacoesBanner (){
+    return (
+
+        <div className={Styles.banner}></div>
+
+    )
+
+
+}
+
+export default DoacoesBanner
