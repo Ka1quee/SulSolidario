c6f6303bbf8c8610aaf8daa2db5adbd0bdf50d7c
Ka1quee <kaiqueoliveira375@gmail.com>
2024-05-16 00:48:43 -0300

    Atualizando noticias e resolvendo bug

--- src/assets/Componentes/UltimasNoticias.jsx
+++ src/assets/Componentes/UltimasNoticias.jsx
@@ -7,7 +7,7 @@ import CardNoticia from './CardNoticia'
 import NoticiaDestaque from './NoticiaDestaque'
 
 function UltimasNoticias() {
-    
+
     const [showElement, setShowElement] = useState(false)
     const showOrHide = () => setShowElement(true);
 
@@ -21,21 +21,21 @@ function UltimasNoticias() {
                 <NoticiaDestaque
                     linkNoticia="https://g1.globo.com/rs/rio-grande-do-sul/" />
 
-<CardNoticia titulo="Pelotas (RS) decreta estado de calamidade pública | CNN PRIME TIME"
-                        video="https://www.youtube.com/embed/M3hfuUNt9hw?"
-                        linkNoticia="https://youtu.be/M3hfuUNt9hw" />
+                <CardNoticia titulo="Nível do rio Guaíba começa a baixar no Rio Grande do Sul"
+                    video="https://www.youtube.com/embed/KgGHO9KW2uE?si=q4b2RGsqYCXcl_Du"
+                    linkNoticia="https://www.youtube.com/watch?v=KgGHO9KW2uE" />
 
-                    <CardNoticia titulo="Nível do Guaíba volta a subir em Porto Alegre (RS) e deve superar recorde histórico"
-                        video="https://www.youtube.com/embed/Z24ANjgMnZs?"
-                        linkNoticia="https://www.youtube.com/embed/Z24ANjgMnZs" />
+                <CardNoticia titulo="'Mar de carros' no Rio Grande do Sul: o que será feito com os veículos destruídos pelas enchentes?"
+                    video="https://www.youtube.com/embed/IKRg1Vw8KvQ?si=1JXvq2S7Vne9vezn"
+                    linkNoticia="https://www.youtube.com/watch?v=IKRg1Vw8KvQ" />
 
-                    <CardNoticia titulo="Tremores de terra são sentidos por moradores de Caxias do Sul (RS)"
-                        video="https://www.youtube.com/embed/ULWHRyUM1V0?si=2FVS2219LpGM6FHp"
-                        linkNoticia="https://www.youtube.com/watch?v=ULWHRyUM1V0" />
+                <CardNoticia titulo="Os moradores que continuam dentro de casas inundadas pelas chuvas no Rio Grande do Sul"
+                    video="https://www.youtube.com/embed/ZAxtBkJgp2I?si=_g-7fL3tHF_pzO1-"
+                    linkNoticia="https://youtu.be/ZAxtBkJgp2I" />
 
-                    <CardNoticia titulo="Biden diz que EUA estão em contato com o Brasil por enchentes no Rio Grande do Sul; Padilha analisa"
-                        video="https://www.youtube.com/embed/7SKE5dKLEHM?si=DzMfZ-Tn4ZCQbVfn"
-                        linkNoticia="https://www.youtube.com/watch?v=7SKE5dKLEHM" />
+                <CardNoticia titulo="Argentina sofre com chuvas que atingiram o Rio Grande do Sul"
+                    video="https://www.youtube.com/embed/HUAcgfWiyQQ?si=3P15D1nN0JJJKGv9"
+                    linkNoticia="https://www.youtube.com/watch?v=HUAcgfWiyQQ" />
 
             </div>
 
